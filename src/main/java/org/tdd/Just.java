ac016148f071b806bf35a1e73a7387d6f3f8318b
Tomasz Bartczak <tomasz.bartczak@pragmatists.pl>
2013-03-21 09:03:51 +0100

    pom.xml and sample test

---
+++ src/main/java/org/tdd/Just.java
@@ -0,0 +1,5 @@
+package org.tdd;
+
+public class Just {
+
+}
---
+++ src/test/java/org/tdd/JustTest.java
@@ -0,0 +1,15 @@
+package org.tdd;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class JustTest {
+
+	@Test
+	public void shouldDo() {
+		assertThat(true).isTrue();
+
+	}
+
+}
